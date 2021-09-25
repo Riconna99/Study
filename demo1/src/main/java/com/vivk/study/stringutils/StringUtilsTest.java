da58554ce2513f8f66ddef94379e56216d39ee16
Riconna99 <riconna@qq.com>
2021-09-25 16:33:34 +0800

    添加StringUtils依赖

---
+++ demo1/src/main/java/com/vivk/study/stringutils/StringUtilsTest.java
@@ -0,0 +1,14 @@
+package com.vivk.study.stringutils;
+
+import org.springframework.util.StringUtils;
+
+public class StringUtilsTest {
+
+    public static void main(String[] args) {
+        String str=" abcd an";
+        System.out.println(StringUtils.trimAllWhitespace(str));
+
+
+    }
+
+}
